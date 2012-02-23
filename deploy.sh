63f2de668dd981a0c572497c174a6e923315c17a
Paul Dunnavant <pdunnavant@gmail.com>
2012-02-23 00:42:14 -0800

    minor fixes for ubuntu

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,14 @@
 
 # Usage: ./deploy.sh [host]
 
-host="${1:-vagrant@vagrant-ubuntu-11-10}"
+host="${1:-vagrant@192.168.0.1}"
+port="${2}"
 
 # The host key might change when we instantiate a new VM, so
 # we remove (-R) the old host key from known_hosts
 ssh-keygen -R "${host#*@}" 2> /dev/null
 
-tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" '
+tar cj . | ssh -o 'StrictHostKeyChecking no' -p ${port} "${host}" '
 sudo rm -rf ~/chef &&
 mkdir ~/chef &&
 cd ~/chef &&
